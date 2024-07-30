f986a5301d696dc44b25f3f6e0c0da7930e2abb1
gerardjr42 <ggarciajr12@gmail.com>
2024-07-30 06:05:00 -0400

    fix middleware

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require("express");
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 //Anime Routes
 const animeController = require("./controllers/animeController.js");
